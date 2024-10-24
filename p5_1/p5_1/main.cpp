03d6b1a9bccc2a28293e9843ec0fd1cb164a57a5
danya-purple02 <iza.darkwoodenn@gmail.com>
2024-10-24 18:33:43 +0300

    task 1 point 3 done

--- p5_1/p5_1/main.cpp
+++ p5_1/p5_1/main.cpp
@@ -3,31 +3,36 @@
 
 using namespace std;
 
-void main() 
+void main()
 {
 	srand(time(NULL));
 
-	int vertex_amount = 1, graph_size = 0;
+	int vertex_amount = 1, graph_size = 0, vertex_number = 0, tmp_flag = 0, dom_vertex = 0, dom_vertex_degree = 0;
 	cout << "input graph size: ";
 	cin >> vertex_amount;
 	cout << endl;
 
-
+	int* vertexes_degrees = new int[vertex_amount];
 	int** G = new int* [vertex_amount];
-	for (int z = 0; z< vertex_amount; z++) 
+	for (int z = 0; z < vertex_amount; z++)
 	{
 		G[z] = new int[vertex_amount];
+		vertexes_degrees[z] = 0;
 	}
+
+	cout << "    ";
+	for (int i = 1; i <= vertex_amount; i++) 
+	{
+		cout << i << " ";
+	}
+	cout << endl << endl;
 
 	for (int i = 0; i < vertex_amount; i++)
 	{
+		cout << i+1 << "   ";
 		for (int j = 0; j < vertex_amount; j++)
 		{
-			if (i == j)
-			{
-				G[i][j] = 0;
-			}
-			else if (j < i) 
+			if (j < i) 
 			{
 				G[i][j] = G[j][i];
 			}
@@ -46,9 +51,48 @@ void main()
 		{
 			if (G[i][j] == 1)
 			{
+				if (i == j) 
+				{
+					graph_size++;
+				}
 				graph_size++;
 			}
 		}
 	}
 	cout << endl << graph_size << endl;
+
+	for (int i = 0; i < vertex_amount; i++)
+	{
+		for (int j = 0; j < vertex_amount; j++)
+		{
+			vertex_number = i + 1;
+			if ((G[i][j] == 1) && (i != j))
+			{
+				tmp_flag++;
+			}
+		}
+		if (tmp_flag == 0) 
+		{
+			cout << vertex_number << " is isolated vertex" << endl;
+		}
+		else if (tmp_flag == 1) 
+		{
+			cout << vertex_number << " is terminal vertex" << endl;
+		}
+		vertexes_degrees[i] = tmp_flag;
+		tmp_flag = 0;
+	}
+
+	for (int i = 0; i < vertex_amount; i++) 
+	{
+		if (vertexes_degrees[i] == (vertex_amount - 1)) 
+		{
+			dom_vertex_degree = vertexes_degrees[i];
+			dom_vertex = i + 1;
+		}
+	}
+	if (dom_vertex != 0) 
+	{
+		cout << "dominant vertex is " << dom_vertex << " and it's degree = " << dom_vertex_degree << endl;
+	}
 }
